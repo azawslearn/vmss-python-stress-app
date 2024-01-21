41e8bb6c78b18259cebacf455706e26e0cd2d725
azawslearn <134204024+azawslearn@users.noreply.github.com>
2024-01-21 15:09:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def cpu_utilization():
     return jsonify(utilization=utilization)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=5000)
